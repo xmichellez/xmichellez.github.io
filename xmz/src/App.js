78277ea3a9f196f44cb7f2fe1ca6b369dcedf62e
Michelle Zhang <xmz@duke.edu>
2023-06-24 18:06:12 -0400

    basic links

--- xmz/src/App.js
+++ xmz/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="title" id="bottom"> */}
+        <h1 className="title">
+          x<span id="hide">inyu&nbsp;</span>m<span id="hide">ichelle&nbsp;</span>z<span id="hide">hang</span>
+        </h1>
+      {/* </header>         */}
+      <h2 className="links">      
+          <a href="https://www.linkedin.com/in/xmichellez/" target="_blank">linkedin </a> 
+          <a href="https://drive.google.com/file/d/10VE_VvvU4ENSBkACbw9Ka1uNAlq_VV3X/view?usp=sharing" target="_blank">resume </a> 
+          <a href="https://github.com/xmichellez" target="_blank">github </a> 
+          <a href="mailto:xinyumzhang@gmail.com" target="_blank">email </a> 
+          <a href="https://www.linkedin.com/in/xmichellez/" target="_blank">fun </a>
+        </h2>
     </div>
   );
 }
