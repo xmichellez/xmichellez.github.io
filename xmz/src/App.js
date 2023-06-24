fbd0a455a3521cde131391c8e99749b1085e74e3
Michelle Zhang <xmz@duke.edu>
2023-06-24 19:17:02 -0400

    favicon

--- xmz/src/App.js
+++ xmz/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <a href="https://drive.google.com/file/d/10VE_VvvU4ENSBkACbw9Ka1uNAlq_VV3X/view?usp=sharing" target="_blank">resume </a> 
           <a href="https://github.com/xmichellez" target="_blank">github </a> 
           <a href="mailto:xinyumzhang@gmail.com" target="_blank">email </a> 
-          <a href="https://www.linkedin.com/in/xmichellez/" target="_blank">fun </a>
+          <a href="https://www.youtube.com/watch?v=qWAagS_MANg&ab_channel=BonApp%C3%A9tit" target="_blank">fun </a>
         </h2>
     </div>
   );
